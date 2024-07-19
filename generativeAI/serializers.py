1225642286680e8004ca5ebd7f6e94da513d4418
KHJ <mo481872@gmail.com>
2024-07-19 15:03:22 +0900

    modify GenAI prompt

--- generativeAI/serializers.py
+++ generativeAI/serializers.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 
 class ImageUploadSerializer(serializers.Serializer):
     init_image = serializers.ImageField()
-    image_strength = serializers.FloatField(default=0.5)
-    text_prompts = serializers.CharField(default="Transform the urban area shown in the attached image into a lush urban forest.\
-                                         Replace the buildings with a variety of trees, plants, and greenery. Include walking paths,\
-                                         benches, and open spaces for people to relax. Ensure the area is filled with diverse vegetation,\
-                                         creating a natural and serene environment within the city.")
+    image_strength = serializers.FloatField(default=0.45)
+    text_prompts = serializers.CharField(default="Detailed, highly photorealistic,\
+                                         and sharply focused scene of a lush, vibrant urban forest landscape.\
+                                         Replace all buildings, roads, and infrastructure with a diverse array of tall trees,\
+                                         dense foliage, winding gravel paths, natural boulders, and flowing streams.\
+                                         Showcase the seamless integration of nature and the built environment in a clear,\
+                                         crisp, and lifelike manner.")
     cfg_scale = serializers.IntegerField(default=7)
     samples = serializers.IntegerField(default=1)
     steps = serializers.IntegerField(default=30)
