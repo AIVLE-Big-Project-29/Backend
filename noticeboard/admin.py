9047b61ec734c92d2ab288924c7a304f393fcfb7
KHJ <mo481872@gmail.com>
2024-07-22 16:14:33 +0900

    add admin page

--- noticeboard/admin.py
+++ noticeboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'created_at', 'updated_at')
+    search_fields = ('title', 'content', 'user__username')
+    list_filter = ('created_at', 'updated_at', 'user')
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'affiliation', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'affiliation')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('profile_image', 'affiliation')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('profile_image', 'affiliation')}),
+    )
